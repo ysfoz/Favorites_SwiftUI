328102d89d5471858e07a22727a316203d4df141
yusuf <ysfozturk3@gmail.com>
2021-11-16 15:08:48 +0100

    created

---
+++ Favorites_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorites_SwiftUI
+//
+//  Created by ysf on 16.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favorites_SwiftUI/Favorites_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorites_SwiftUIApp.swift
+//  Favorites_SwiftUI
+//
+//  Created by ysf on 16.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Favorites_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
